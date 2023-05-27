9ec30854ecac93e0642c55f0d00644dccd98e055
antoniomauricio16 <antoniomauriciotecg@gmail.com>
2023-05-27 14:12:14 -0300

    rotas do fornt

--- src/js/indexlogin.js
+++ src/js/indexlogin.js
@@ -3,6 +3,50 @@ const api = axios.create({
   baseURL: "http://localhost:8081/"
 })
 
+let page = 1
+let pages = 0
+let totalRecados =0
+
+
+function paginaAnterior(){
+  mostrarRecados()
+  if(page > 1){
+      page--
+      mostrarRecados(page)
+  }
+}
+
+function proximaPagina(){
+  if(pages > page)
+  page++
+  mostrarRecados(page)
+}
+
+function mostrarRecados(page){
+  document.getElementById("mostrarRecados").style.display = "block"
+
+  
+  api.get("/recados", {params: { page }})
+
+  .then((res) => {
+      const recados = res.data.data;
+      totalRecados = res.data.total
+      pages = res.data.pages
+
+      let cards = document.getElementById("messagesList");
+      cards.innerHTML = "";
+      recados.forEach((recado) => {
+          cards.innerHTML += `<div class="card"> <span>${recado.titulo}</span><p>${recado.recado}</p><button onclick="mostrarEdicaoRecados(\'${recado.titulo}\', \'${recado.recado}\')">Editar</button></div>`
+      });
+  })
+  .catch((err)=>{
+      alert(err)
+  })
+
+  
+}
+
+
 //criar usuario
 function criarUsuario(event){
   event.preventDefault();
@@ -72,50 +116,4 @@ function criarRecados(event){
 function mostrarEdicaoRecados(titulo, recado){
  
 }
-
-
-
-let page = 1
-let pages = 0
-let totalRecados =0
-
-
-function paginaAnterior(){
-  mostrarRecados()
-  if(page > 1){
-      page--
-      mostrarRecados(page)
-  }
-}
-
-function proximaPagina(){
-  if(pages > page)
-  page++
-  mostrarRecados(page)
-}
-
-function mostrarRecados(page){
-  document.getElementById("mostrarRecados").style.display = "block"
-
-  
-  api.get("/recados", {params: { page }})
-
-  .then((res) => {
-      const recados = res.data.data;
-      totalRecados = res.data.total
-      pages = res.data.pages
-
-      let cards = document.getElementById("messagesList");
-      cards.innerHTML = "";
-      recados.forEach((recado) => {
-          cards.innerHTML += `<div class="card"> <span>${recado.titulo}</span><p>${recado.recado}</p><button onclick="mostrarEdicaoRecados(\'${recado.titulo}\', \'${recado.recado}\')">Editar</button></div>`
-      });
-  })
-  .catch((err)=>{
-      alert(err)
-  })
-
-  
-}
-
 
